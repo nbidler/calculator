65eba2f9c106e833ff55188d2d786185266b9b54
Nicholas Bidler <nomandoestheimpossible@gmail.com>
2016-02-03 14:00:41 -0800

    calculator v0.5 - Nick Bidler

--- script.js
+++ script.js
@@ -3,7 +3,14 @@
  */
 
 function inputHandler(type, value, item) {
-    $('#display_area').html(value);
+    switch (value) {
+        case undefined:
+            $('#display_area').html("");
+            break;
+        default:
+            $('#display_area').html(value);
+            break;
+    }
 }
 
 var my_calculator = new calculator(inputHandler);
@@ -12,6 +19,17 @@ $(document).ready(function(){
     //on click, get contained value
     $('.item').click(function(){
         var val = $(this).text();
-        my_calculator.addItem(val);
+        console.log("btn clicked : ", val)
+        switch (val) {
+            case 'AC':
+                my_calculator.allClear();
+                break;
+            case 'C':
+                my_calculator.clear();
+                break;
+            default:
+                my_calculator.addItem(val);
+                break;
+        }
     });
 });
